ae58727ee11f40acd4c9045f77d55cf4c08a79c5
FilipStudený <BluesyPompanno@seznam.cz>
2024-01-18 11:33:03 +0100

    Fix for table pagination

--- Client/obj/Debug/net7.0/Ecommerce.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net7.0/Ecommerce.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00aacc4416e3e2c47d9828fb50bdb127af17d984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/obj/Debug/net7.0/Ecommerce.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net7.0/Ecommerce.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00aacc4416e3e2c47d9828fb50bdb127af17d984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3678091eb9efbd06f96bf03b6ff2adfb18dc22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
